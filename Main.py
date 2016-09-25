37fe81b9d4923ba34feafe150ab8ca55ddaae9fd
Tianshu Ren <tren@UNIX6.ANDREW.CMU.EDU>
2016-09-25 16:31:40 -0400

    some scripts

--- Main.py
+++ Main.py
@@ -32,8 +32,8 @@ def plotError(network, training_loss, cv_loss, loss_label):
 	return fig
 
 if __name__ == '__main__':
-	[X_train, y_train] = loadData("/Users/tianshuren/Google Drive/2016 Fall/10807/Code/HW1/data/digitstrain.txt")
-	[X_val, y_val] = loadData("/Users/tianshuren/Google Drive/2016 Fall/10807/Code/HW1/data/digitsvalid.txt")
+	[X_train, y_train] = loadData("HW1/data/digitstrain.txt")
+	[X_val, y_val] = loadData("HW1/data/digitsvalid.txt")
 
 	myNet = Network.Network()
 	myNet.setLayer(3, [[784, "Input"], [500, "Sigmoid"], [10, "Softmax"]])
@@ -41,13 +41,14 @@ if __name__ == '__main__':
 	myNet.initNetwork()
 	myNet.setMomentum(0.5)
 	myNet.setL2Weight(0.0)
+	myNet.setDropOutRate(0.5)
 	epochs = 200
 
 	[loss, closs, vloss, vcloss] = myNet.trainAndValidate(X_train, y_train, X_val, y_val, epochs)
 
 	fig = plotError(myNet, loss, vloss, "Cross-entropy Loss")
-	fig.savefig('../HW1/Results/ce_loss_lr1e-2_mo5e-1_hl5e+2.png', format='png')
+	fig.savefig('Results/ce_loss_lr1e-2_mo5e-1_hl5e+2_dr5e-1.png', format='png')
 	fig = plotError(myNet, closs, vcloss, "Misclassification Error")
-	fig.savefig('../HW1/Results/mc_loss_lr1e-2_mo5e-1_hl5e+2.png', format='png')
+	fig.savefig('Results/mc_loss_lr1e-2_mo5e-1_hl5e+2_dr5e-1.png', format='png')
 	# fig = myNet.visualizeLayer(1)
 	# fig.savefig('../HW1/Results/visualize_weight.png', format='png')
