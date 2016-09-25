77c219ce97559a90823fbbea4221bc79561f4395
Tianshu Ren <tianshu.r@gmail.com>
2016-09-24 22:30:43 -0400

    Minor changes

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from HW1.NetworkStructure import Network
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 
 def loadData(filename):
 	X = list()
@@ -32,9 +32,6 @@ def plotError(network, training_loss, cv_loss, loss_label):
 	return fig
 
 if __name__ == '__main__':
-	matplotlib.use('Agg')
-	import matplotlib.pyplot as plt
-
 	[X_train, y_train] = loadData("/Users/tianshuren/Google Drive/2016 Fall/10807/Code/HW1/data/digitstrain.txt")
 	[X_val, y_val] = loadData("/Users/tianshuren/Google Drive/2016 Fall/10807/Code/HW1/data/digitsvalid.txt")
 
