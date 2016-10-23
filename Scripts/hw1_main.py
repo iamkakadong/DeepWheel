745bb19f7b3227f3339230eeee7041172e0e3709
Tianshu Ren <tianshu.r@gmail.com>
2016-10-23 00:38:26 -0400

    Deprecated due to refactoring

--- Scripts/hw1_main.py
+++ Scripts/hw1_main.py
@@ -1,23 +1,24 @@
 import os.path
 
 import matplotlib.pyplot as plt
+from traits.util.deprecated import deprecated
 
 from NetworkStructure import Network
 from Helper import Loader
 from Helper import String
 
-
-def plotError(network, training_loss, cv_loss, loss_label):
+def plot_error(network, training_loss, cv_loss, loss_label):
 	fig = plt.figure()
-	plt.plot(training_loss, label = 'Training loss')
+	plt.plot(training_loss, label='Training loss')
 	plt.hold(True)
-	plt.plot(cv_loss, label = 'Cross-validation loss')
+	plt.plot(cv_loss, label='Cross-validation loss')
 	plt.xlabel('Epoch Number')
 	plt.ylabel(loss_label)
-	plt.title(network.printStruct())
+	plt.title(network.print_struct())
 	plt.legend()
 	return fig
 
+@deprecated
 def cross_validation():
 	cv_res = list()
 	params = list()
@@ -44,6 +45,7 @@ def cross_validation():
 
 	return cv_res
 
+@deprecated
 def subroutine(param):
 	myNet = Network.Network()
 	learning_rate = param['learning_rate']
@@ -55,20 +57,20 @@ def subroutine(param):
 
 	# cv_res = param['cv_res']
 
-	myNet.setLayer(3, [[784, "Input"], [hidden_unit, "Sigmoid"], [10, "Softmax"]])
-	myNet.setLearningRate(learning_rate)
-	myNet.setMomentum(momentum)
-	myNet.setL2Weight(regularization)
-	myNet.setDropOutRate(dropout_rate)
+	myNet.set_layer(3, [[784, "Input"], [hidden_unit, "Sigmoid"], [10, "Softmax"]])
+	myNet.set_learning_rate(learning_rate)
+	myNet.set_momentum(momentum)
+	myNet.set_l2_reg(regularization)
+	myNet.set_dropout_rate(dropout_rate)
 	myNet.initNetwork()
 
-	name = myNet.getName() + '_ep_' + String.sciFormat(epochs)
+	name = myNet.get_name() + '_ep_' + String.sci_format(epochs)
 	if not os.path.isfile('Results/ce_loss_' + name + '.png'):
-		[loss, closs, vloss, vcloss] = myNet.trainAndValidate(X_train, y_train, X_val, y_val, epochs)
+		[loss, closs, vloss, vcloss] = myNet.train_and_validate(X_train, y_train, X_val, y_val, epochs)
 
-		fig = plotError(myNet, loss, vloss, "Cross-entropy Loss")
+		fig = plot_error(myNet, loss, vloss, "Cross-entropy Loss")
 		fig.savefig('Results/ce_loss_' + name + '.png', format='png')
-		fig = plotError(myNet, closs, vcloss, "Misclassification Error")
+		fig = plot_error(myNet, closs, vcloss, "Misclassification Error")
 		fig.savefig('Results/mc_loss_' + name + '.png', format='png')
 		print "cross-validation result for " + name + ":\n"
 		print "\ttraining cross-entropy loss: %0.3f\n" % loss[-1]
@@ -79,29 +81,29 @@ def subroutine(param):
 		return [loss,closs,vloss,vcloss,name]
 
 if __name__ == '__main__':
-	[X_train, y_train] = Loader.loadData("../data/digitstrain.txt")
-	[X_val, y_val] = Loader.loadData("../data/digitsvalid.txt")
-	[X_test, y_test] = Loader.loadData("../data/digitstest.txt")
+	[X_train, y_train] = Loader.load_data("../data/digitstrain.txt")
+	[X_val, y_val] = Loader.load_data("../data/digitsvalid.txt")
+	[X_test, y_test] = Loader.load_data("../data/digitstest.txt")
 
 	myNet = Network.Network()
 	# cv_res = cross_validation()
 	# with open('cv_result', 'wb') as f:
 	# 	pickle.dump(cv_res, f)
 
-	myNet.setLayer(4, [[784, "Input"], [200, "Sigmoid"], [200, "Sigmoid"], [10, "Softmax"]])
-	myNet.setLearningRate(0.1)
-	myNet.setMomentum(0.5)
-	myNet.setL2Weight(0.1)
-	myNet.setDropOutRate(0.0)
+	myNet.set_layer(4, [[784, "Input"], [200, "Sigmoid"], [200, "Sigmoid"], [10, "Softmax"]])
+	myNet.set_learning_rate(0.1)
+	myNet.set_momentum(0.5)
+	myNet.set_l2_reg(0.1)
+	myNet.set_dropout_rate(0.0)
 	myNet.initNetwork()
 	epochs = 250
 	#
-	[loss, closs, vloss, vcloss] = myNet.trainAndValidate(X_train, y_train, X_test, y_test, epochs)
+	[loss, closs, vloss, vcloss] = myNet.train_and_validate(X_train, y_train, X_test, y_test, epochs)
 	#
-	fig = plotError(myNet, loss, vloss, "Cross-entropy Loss")
+	fig = plot_error(myNet, loss, vloss, "Cross-entropy Loss")
 	fig.savefig('../Results/HW1/ce_two_layer_best.png', format='png')
-	fig = plotError(myNet, closs, vcloss, "Misclassification Error")
+	fig = plot_error(myNet, closs, vcloss, "Misclassification Error")
 	fig.savefig('../Results/HW1/mc_two_layer_best.png', format='png')
-	fig = myNet.visualizeLayer(1)
+	fig = myNet.visualize_layer(1)
 	fig.savefig('../Results/HW1/visualize_weight_two_layer_best.png', format='png')
 
